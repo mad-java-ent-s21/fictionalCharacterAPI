081e176e6760a5b9d813ad7cf5a418b237693f04
Walker Hurd <whurd@madisoncollege.edu>
2021-04-07 17:18:01 -0500

    added a test class

---
+++ src/test/java/TestServiceClient.java
@@ -0,0 +1,23 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import entity.Character;
+
+import org.junit.Test;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestServiceClient {
+
+    @Test
+    public void fictionalCharacterAPIJSONCharacter() throws Exception {
+        Client client = ClientBuilder.newClient();
+        WebTarget target =
+                client.target("http://localhost:8080/fictionalCharacterAPI_war/service/name/neo");
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        Character character = mapper.readValue(response, Character.class);
+        assertEquals("neo", character.getName());
+        //assertEquals("???", response);
+    }
+
+}
