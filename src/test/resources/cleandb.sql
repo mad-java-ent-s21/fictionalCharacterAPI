2923b1732c79e88af89941767c5849bcefcffcdd
Walker Hurd <whurd@madisoncollege.edu>
2021-04-09 18:30:43 -0500

    trying to get the tests to work

---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,4 @@
+delete from characters;
+INSERT INTO `characters` VALUES (1,'Gandalf','book','Lord Of The Rings','Maia','Good'),(2,'Ripley','movie','Alien','Human','Good'),(3,'Luke Skywalker','movie','Star Wars','Human','Good'),(4,'Neo','movie','The Matrix','Human','Good');
+
+
