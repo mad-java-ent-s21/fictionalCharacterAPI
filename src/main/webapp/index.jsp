ea3580344f4012ebaffb36da3124a577b09df1ea
Christopher <cvang13@madisoncollege.com>
2021-04-12 13:51:50 -0500

    changed h2 of index page

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!!!!!!!!!!!!!!!!!</h2>
+<h2>Fictional Character API</h2>
 </body>
 </html>
--- src/test/java/team/service/TestServiceClient.java
+++ src/test/java/team/service/TestServiceClient.java
@@ -1,8 +1,14 @@
 package team.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import team.entity.Character;
 
 import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,12 +18,12 @@ public class TestServiceClient {
     public void fictionalCharacterAPIJSONCharacter() throws Exception {
         /*Client client = ClientBuilder.newClient();
         WebTarget target =
-                client.target("http://localhost:8080/fictionalCharacterAPI_war/service/name/neo");
+                client.target("http://localhost:8080/fictionalCharacterAPI_war/service/name/4");
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         Character character = mapper.readValue(response, Character.class);
-        assertEquals("neo", character.getName());
-        //assertEquals("???", response);*/
+        assertEquals("neo", character.getName());*/
+//        assertEquals("???", response);
     }
 
 }
